63ecc560ab0040b248c190b662bd01bc37e3fab9
Eric <eric.henschel7@gmail.com>
2020-04-16 02:01:27 -0400

    euler

--- viscFluid.py
+++ viscFluid.py
@@ -8,6 +8,8 @@
 # to -- (s) initial time
 # mu -- (Pa s) dynamic viscosity
 # r -- (m) radius
+# Re -- Reynolds number
+
 
 import numpy as np
 
@@ -20,58 +22,108 @@ class calc:
         self.vo = 5.0 #m/s -- initial velocity
         self.yo = 0.0 #m -- initial position
         self.to = 0.0 #s -- initial time
+        self.density = 1000 #kg/m3
         self.mu = 8.9 * 10**(-4) #Pa s -- viscosity (water at 25 deg celsius)
         self.radius = 0.01 #m (1 cm) -- radius
     
-    # params:: r - radius, mu - viscosity
+    # Reynolds number using 
+    #density (kg/m^3) * fluid flow velocity (m/s) * 
+    #length ((m) :: i.e. the diameter of our sphere) / viscosity (Pa s)
+    def Re(self):
+        return self.density * (1) * (self.radius * 2) / self.mu
+    
+    def Cd(self):
+        return 24 / self.Re() + 6/(1+np.sqrt(self.Re())) + 0.4
+
+    # accounting for half the surface area of a sphere,
+    # since the majority of the friction felt will be determined
+    # by only one side
+    def A(self, r):
+        return (0.5) * 4 * np.pi * (self.radius**2)
+
+    def b(self):
+        return (0.5) * self.density * self.Cd() * self.A(self.radius)
+
+    """# params:: r - radius, mu - viscosity
     def b(self, r, mu):
         return (6 * np.pi * self.mu * self.radius)
+        #return (0.5) * self.density * """
 
     # y'' = f(v) = acceleration
     def f(self, v):
-        zo = self.b(self.radius, self.mu) * v / self.mass
+        zo = self.b() * v / self.mass
         return (-self.g - zo)
 
     # y = velocity
     def euler_vel(self):
-        time = []
+        time = [self.to]
         vel = [self.vo]
         for i in range(0, 50):
             v = vel[i]
-            a = f(v)
+            a = self.f(v)
             vn = v + a * self.step
-            vel.append(vn)
-            time.append(self.step*i)
+            if vn <=0:
+                vel.append(0)
+            else:
+                vel.append(vn)
+
+            time.append(self.step*float(i+1))
 
         return time, vel
             
     def euler_pos(self, v):
-        time = [0.0]
-        pos = [self.yo]
+        pos = [self.xo]
         for i in range(0, 50):
-				y = pos[i]
-				f = v[i]
-				yn = 
+            y = pos[i]
+            f = v[i]
+            yn = y + f * self.step
+            pos.append(yn)
+        return pos
 
-    #def rkMethod(self):
+    #def rk_Method(self):
 
     # analytical equation for velocity
-    def vel(t):
-	velocity = []
-	for i in range(len(t)):
-		zo = -5.0 * self.mass / self.b(self.radius, self.mu)
-		z1 = -self.g * self.mass**2 / (self.b(self.radius, self.mu)**2)
-		e = np.exp(-self.b(self.radius, self.mu) * t / self.mass)
-		z2 = 5.0 * self.mass / self.b(self.radius, self.mu)
-		z3 = self.g * self.mass**2 / (self.b(self.radius, self.mu)**2)
-		z4 = -self.g * self.mass * t / self.b(self.radius, self.mu)
-		v =  (zo + z1) * e + z2 + z3 + z4
-		velocity.append(v)
+    def pos(self, t):
+        position = [self.yo]
+        for i in range(len(t)):
+            zo = (-5.0) * self.mass / self.b()
+            z1 = -self.g * (self.mass**2) / (self.b()**2)
+            e = np.exp(-self.b() * t[i] / self.mass)
+            z2 = (-5.0 * self.mass) / self.b()
+            z3 = (-self.g * self.mass**2) / (self.b()**2)
+            z4 = (-self.g * self.mass * t[i]) / self.b()
+            v = ((zo + z1) * e + z2 + z3 + z4)
+            if v<=0:            
+                position.append(0)
+            else:
+                position.append(v)
+
+        return position
+
+    def vel(self, t):
+        velocity = []
+        for i in range(len(t)):
+            zo = (-5.0) * self.mass / self.b()
+            z1 = -self.g * (self.mass**2) / (self.b()**2)
+            z2 = (-self.b()/self.mass)
+            e = np.exp(-self.b() * t[i] / self.mass)
+            z3 = -self.g * self.mass / self.b()
+            v = (zo + z1) * z2 * e + z3
+            velocity.append(v)
+        return velocity
+
+    
+    def euler_err(self):
+        v = self.euler_vel()[1]
+        t = self.euler_vel()[0]
+        est = self.vel(t)
+        print(len(v), len(est))
+        print(" Time ", " Pred_Velocity ", " Analytic_Velocity ")
+        for i in range(len(t)):
+            e = np.abs(v[i]-est[i])
+            print(t[i], v[i], est[i], e)
 
 
 if __name__=="__main__":
-
-	C = calc()
-	v = C.euler_vel()[1]
-	for i in range(len(v)):
-		print(v[i])
+    C = calc()
+    C.euler_err()
